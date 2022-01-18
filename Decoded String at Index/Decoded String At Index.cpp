143bf8f732579d7f9543a045b413ef0f6fa291f4
Eric Washington <Mrkimjarrow95@gmail.com>
2022-01-17 20:28:14 -0600

    I am going to have to rethink my whole approach to the Decoded string problem.

---
+++ Decoded String at Index/Decoded String At Index.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <string>
+
+
+using namespace std;
+
+string decodeAtIndex(string s, int k){
+    
+    }
+
+
+int main(){
+
+    cout << decodeAtIndex("a2b3c4d5e6f7g8h9",10) << endl;
+    //cout << decodeAtIndex("leet2code3",10) << endl;
+
+    return 0;
+}
